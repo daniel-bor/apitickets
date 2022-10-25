295d94d2bb52684e3c75290665626ed284736c91
Emanuel Barillas <emanuel.barillas@sisnovasa.com>
2022-10-25 10:30:29 -0600

    Primer Commit

---
+++ Controllers/FileController.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+
+
+namespace DesafioDotNET.Controllers
+{
+    [ApiController]
+    [Route("api/[controller]")]
+    public class FileController : ControllerBase
+    {
+        [HttpGet]
+        public async Task<IActionResult> List(int idempresa)
+        {
+            return Ok(new List<dynamic>()
+            {
+                new
+                {
+                    Nombre = "Hola mundo"
+                }
+            });
+        }
+    }
+}
+
